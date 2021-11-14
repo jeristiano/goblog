fab394f675ed74c48c95da0fe3605fab059ec965
jersitiano <jeremykuang@foxmail.com>
2021-11-14 18:28:52 +0800

    创建数据表

--- main.go
+++ main.go
@@ -49,6 +49,16 @@ func checkError(err error) {
 	}
 }
 
+func createTables() {
+	createArticlesSQL := `CREATE TABLE IF NOT EXISTS articles(
+		id bigint(20) PRIMARY KEY AUTO_INCREMENT NOT NULL,
+		title varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+		body longtext COLLATE utf8mb4_unicode_ci
+	); `
+	_, err := db.Exec(createArticlesSQL)
+	checkError(err)
+}
+
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Hello, 欢迎来到 goblog！</h1>")
 }
@@ -160,6 +170,7 @@ func articlesCreateHandler(w http.ResponseWriter, r *http.Request) {
 }
 func main() {
 	initDB()
+	createTables()
 	router.HandleFunc("/", homeHandler).Methods("GET").Name("home")
 	router.HandleFunc("/about", aboutHandler).Methods("GET").Name("about")
 	router.HandleFunc("/articles/{id:[0-9]+}", articlesShowHandler).Methods("GET").Name("articles.show")
