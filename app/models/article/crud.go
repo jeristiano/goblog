4cff562c53fa089eddb901e581ebf808cee95b46
jersitiano <jeremykuang@foxmail.com>
2022-06-02 06:05:58 +0800

    用户的文章

--- app/models/article/crud.go
+++ app/models/article/crud.go
@@ -17,6 +17,14 @@ func Get(idstr string) (Article, error) {
 	return article, nil
 }
 
+func GetByUserID(uid string) ([]Article, error) {
+	var articles []Article
+	if err := model.DB.Where("user_id = ?", uid).Preload("User").Find(&articles).Error; err != nil {
+		return articles, err
+	}
+	return articles, nil
+}
+
 // GetAll 获取全部文章
 func GetAll() ([]Article, error) {
 	var articles []Article
